a09b443ff9ea30a0203738a5a99504baf637925d
Sidharth-1996 <sharmasidharth784@gmail.com>
2026-01-29 19:23:28 +0530

    deployment....

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-app.options("*", cors());
+app.options("/*", cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
